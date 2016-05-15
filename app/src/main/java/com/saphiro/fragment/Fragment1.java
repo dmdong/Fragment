aacd8d2db434302240a9184aee9e299e5054f8b7
dmdong <dong.manh@hotmail.com>
2016-05-15 21:02:46 +0700

    Initial commit

---
+++ app/src/main/java/com/saphiro/fragment/Fragment1.java
@@ -0,0 +1,58 @@
+package com.saphiro.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by Saphiro on 5/15/2016.
+ */
+public class Fragment1 extends Fragment{
+
+
+    @BindView(R.id.fragmentTitle)
+    TextView fragmentTitle;
+    @BindView(R.id.editText)
+    EditText editText;
+    @BindView(R.id.btnnext)
+    Button btnNext;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View root = inflater.inflate(R.layout.fragment_fragment1, container, false);
+        ButterKnife.bind(this, root);
+
+        Bundle bundle = getArguments();
+        fragmentTitle.setText(bundle.getString("title"));
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Fragment1 f2 = new Fragment1();
+                Bundle bundle2 = new Bundle();
+                bundle2.putString("title", editText.getText().toString());
+                f2.setArguments(bundle2);
+
+                ((MainActivity) getActivity()).addFragment(f2, true);
+            }
+
+        });
+
+        return root;
+    }
+
+}
+
+
---
+++ app/src/main/java/com/saphiro/fragment/MainActivity.java
@@ -0,0 +1,32 @@
+package com.saphiro.fragment;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment1 f1 = new Fragment1();
+        Bundle bundle = new Bundle();
+        bundle.putString("title", "1st Fragment");
+        f1.setArguments(bundle);
+
+        addFragment(f1, true);
+    }
+
+    public void addFragment(Fragment fragment, boolean addedToBackStack) {
+
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
+
+    }
+}
